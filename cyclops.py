173811f63a50fb11f2ea45c4f0bf593cbe6a27a2
Geoffroy Givry <geoff.givry@gmail.com>
2017-05-01 23:02:21 +0800

    added login page

--- cyclops.py
+++ cyclops.py
@@ -14,6 +14,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
 @app.route('/polyphemus')
 def polyphemus():
     subs = [x for x in mongo.db.dailies_submissions.find()]
