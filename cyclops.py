d37e114c9cca32dc6bd0869a4bd2fafe9d3b3eb8
Geoffroy Givry <geoff.givry@gmail.com>
2017-05-02 22:27:36 +0800

    login system added

--- cyclops.py
+++ cyclops.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
+import bcrypt
 import cyc_config as cfg
 
 app = Flask(__name__)
@@ -11,19 +12,54 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if 'username' in session:
+        return render_template("index.html", user_session=session['username'])
+    else:
+        return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        users = mongo.db.users
+        login_user = users.find_one({'name': request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+
+        return 'Invalid username/password combination'
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name': request.form['username'], 'password': hashpass, 'email': request.form['email']})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
 
 
 @app.route('/polyphemus')
 def polyphemus():
-    subs = [x for x in mongo.db.dailies_submissions.find()]
-    return render_template("polyphemus.html", subs=subs)
+    if 'username' in session:
+        subs = [x for x in mongo.db.dailies_submissions.find()]
+        return render_template("polyphemus.html", subs=subs)
+    else:
+        return render_template("login.html")
 
 
 if __name__ == "__main__":
+    app.secret_key = cfg.FLASK_APP_SECRET_KEY
     app.run(debug=True)
