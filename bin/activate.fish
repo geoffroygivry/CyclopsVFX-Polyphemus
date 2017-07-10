51a4493543c60ac470f8ca1c5f721736ba4bad44
geoff.givry <geoff.givry@gmail.com>
2017-07-09 23:19:02 -0400

    added task tooltip on shot/asset assignment on user admin page

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/geoff/Dropbox/01_DEV/02_webDev/cyclops-vfx/polyphemus"
+set -gx VIRTUAL_ENV "/home/cabox/workspace/CyclopsVFX-Polyphemus"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
