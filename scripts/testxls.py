8e2c52e35c01c98fd7a18ccac2296c6dd8fe6c16
geoffroygivry <geoff.givry@gmail.com>
2017-07-17 23:43:30 +0800

    method populate_shows is working

--- scripts/testxls.py
+++ scripts/testxls.py
@@ -1,36 +1,36 @@
 import xlrd
 
-workbook = xlrd.open_workbook('/home/cabox/workspace/cyc-template.xlsx')
-worksheet = workbook.sheet_by_index(0)
-first_row = [] # The row where we stock the name of the column
-for col in range(worksheet.ncols):
-    first_row.append( worksheet.cell_value(0,col) )
-# transform the workbook to a list of dictionaries
-data =[]
-for row in range(1, worksheet.nrows):
-    elm = {}
-    for col in range(worksheet.ncols):
-        elm[first_row[col]]=worksheet.cell_value(row,col)
-    data.append(elm)
-print(data)
 
-
-class xls_to_mongodb():
+class xls_to_mongodb2():
     def __init__(self, path_to_xls, db):
         self.db = db
         self.workbook = xlrd.open_workbook(path_to_xls)
-        
+
     def populate_shows(self):
         worksheet = self.workbook.sheet_by_index(0)
-        first_row = [] # The row where we stock the name of the column
+        first_row = []  # The row where we stock the name of the column
         for col in range(worksheet.ncols):
-            first_row.append( worksheet.cell_value(0,col) )
+            first_row.append(worksheet.cell_value(0, col))
         # transform the workbook to a list of dictionaries
-        data =[]
+        data = []
         for row in range(1, worksheet.nrows):
             elm = {}
             for col in range(worksheet.ncols):
-                elm[first_row[col]]=worksheet.cell_value(row,col)
+                elm[first_row[col]] = worksheet.cell_value(row, col)
+                elm['sequences'] = []
+                elm['ptuid'] = 1
+                elm['active'] = False
+                elm['assets'] = []
             data.append(elm)
         for document in data:
-            print(document)
+            if db.shows.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+                print("let's insert this bad boy! {}".format(document['name']))
+                print("doing it...")
+                db.shows.insert_one(document)
+                print("bad boy inserted!")
+
+
+load_xls = xls_to_mongodb2("/home/geoff/Downloads/cyc-template.xlsx", db)
+load_xls.populate_shows()
