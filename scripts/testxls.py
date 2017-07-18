3b856655c44bdaed61b5a6ad252e2a3f8e47617c
geoff.givry <geoff.givry@gmail.com>
2017-07-18 07:59:03 -0400

    wip on testxls.py

--- scripts/testxls.py
+++ scripts/testxls.py
@@ -1,13 +1,14 @@
 import xlrd
+from scripts import db_actions as dba
 
 
-class xls_to_mongodb2():
+class xls_to_mongodb():
     def __init__(self, path_to_xls, db):
         self.db = db
         self.workbook = xlrd.open_workbook(path_to_xls)
-
-    def populate_shows(self):
-        worksheet = self.workbook.sheet_by_index(0)
+        
+    def pull_data(self, sheet_page):
+        worksheet = self.workbook.sheet_by_index(sheet_page)
         first_row = []  # The row where we stock the name of the column
         for col in range(worksheet.ncols):
             first_row.append(worksheet.cell_value(0, col))
@@ -17,20 +18,29 @@ class xls_to_mongodb2():
             elm = {}
             for col in range(worksheet.ncols):
                 elm[first_row[col]] = worksheet.cell_value(row, col)
-                elm['sequences'] = []
-                elm['ptuid'] = 1
-                elm['active'] = False
-                elm['assets'] = []
             data.append(elm)
+        return data
+
+    def populate_shows(self):
+        data = self.pull_data(0)
         for document in data:
-            if db.shows.find({"name": document['name']}).count() > 0:
+            if self.db.shows.find({"name": document['name']}).count() > 0:
                 print("{} already exists!".format(document['name']))
             else:
-                print("let's insert this bad boy! {}".format(document['name']))
-                print("doing it...")
-                db.shows.insert_one(document)
-                print("bad boy inserted!")
+                dba.create_show(document['long_name'], document['name'])
+                print("{} will be inserted".format(document['name']))
+                
+                
+    def populate_seqs(self):
+        data = self.pull_data(1)
+        for document in data:
+            if self.db.seqs.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+#                 dba.create_show(document['long_name'], document['name'])
+                print("{} will be inserted".format(document['name']))
 
 
-load_xls = xls_to_mongodb2("/home/geoff/Downloads/cyc-template.xlsx", db)
-load_xls.populate_shows()
+
+# load_xls = xls_to_mongodb("/home/geoff/Downloads/cyc-template.xlsx", db)
+# load_xls.populate_shows()
