933af62f368d7007b3506b6d6b460c0792912301
geoffroygivry <geoff.givry@gmail.com>
2017-07-20 23:53:04 +0800

    xls_to_mongodb wip

--- scripts/testxls.py
+++ scripts/testxls.py
@@ -1,11 +1,25 @@
 import xlrd
 from scripts import db_actions as dba
+import datetime
 
 
 class xls_to_mongodb():
+    """ Description:
+        Class that takes the CyclopsVFX's Excel template and populate the db in bulk.
+
+        Example of usage:
+
+        xx = xls.xls_to_mongodb("/home/geoff/Downloads/cyc-template.xlsx", db)
+        xx.populate_shots()
+    """
     def __init__(self, path_to_xls, db):
         self.db = db
         self.workbook = xlrd.open_workbook(path_to_xls)
+
+    def convert_xlsDate_to_datetime(self, xls_date):
+        year, month, day, hour, minute, second = xlrd.xldate_as_tuple(xls_date, self.workbook.datemode)
+        iso_date = datetime.datetime(year, month, day, hour, minute, second).isoformat()
+        return iso_date
 
     def pull_data(self, sheet_page):
         worksheet = self.workbook.sheet_by_index(sheet_page)
@@ -45,9 +59,9 @@ class xls_to_mongodb():
             if self.db.shots.find({"name": document['name']}).count() > 0:
                 print("{} already exists!".format(document['name']))
             else:
-                dba.create_shot(document['show'], document['seq'], document['name'], frame_in=int(document.get('frame_in', 1001)), frame_out=int(document.get('frame_out', 1001)), status=document['status'], target_date="{}T18:00:00.292000".format(document['target_date']))
+                dba.create_shot(document['show'], document['seq'], document['name'], frame_in=int(document.get('frame_in', 1001)), frame_out=int(document.get('frame_out', 1001)), status=document['status'], target_date=self.convert_xlsDate_to_datetime(document['target_date']))
                 print("{} is inserted".format(document['name']))
-                
+
     def populate_assets(self):
         data = self.pull_data(3)
         for document in data:
@@ -58,8 +72,13 @@ class xls_to_mongodb():
                     hero_type = True
                 else:
                     hero_type = False
-                dba.create_asset(document['show'], document['name'], document['type'], hero_type, document['target_date'])
-#                 print(document['show'], document['name'], document['type'], hero_type, document['target_date'])
+                dba.create_asset(document['show'], document['name'], document['type'], hero_type, self.convert_xlsDate_to_datetime(document['target_date']))
+
+    def populate_all(self):
+        self.populate_shows()
+        self.populate_seqs()
+        self.populate_shots()
+        self.populate_assets()
 
 
 """
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,6 @@
 import datetime
+import xlrd
+from scripts import db_actions as dba
 
 
 def convert_isotime_to_datetime(isotime_format):
@@ -82,3 +84,82 @@ def get_users_from_shot(db, shot_name):
     tasks = shot.get('tasks')
     users = [x['assignee'] for x in tasks]
     return users
+
+
+class xls_to_mongodb():
+    """ Description:
+        Class that takes the CyclopsVFX's Excel template and populate the db with entities in bulk.
+
+        Example of usage:
+
+        xx = xls.xls_to_mongodb("/home/geoff/Downloads/cyc-template.xlsx", db)
+        xx.populate_shots()
+    """
+
+    def __init__(self, path_to_xls, db):
+        self.db = db
+        self.workbook = xlrd.open_workbook(path_to_xls)
+
+    def convert_xlsDate_to_datetime(self, xls_date):
+        year, month, day, hour, minute, second = xlrd.xldate_as_tuple(xls_date, self.workbook.datemode)
+        iso_date = datetime.datetime(year, month, day, hour, minute, second).isoformat()
+        return "{}.000000".format(iso_date)
+
+    def pull_data(self, sheet_page):
+        worksheet = self.workbook.sheet_by_index(sheet_page)
+        first_row = []  # The row where we stock the name of the column
+        for col in range(worksheet.ncols):
+            first_row.append(worksheet.cell_value(0, col))
+        # transform the workbook to a list of dictionaries
+        data = []
+        for row in range(1, worksheet.nrows):
+            elm = {}
+            for col in range(worksheet.ncols):
+                elm[first_row[col]] = worksheet.cell_value(row, col)
+            data.append(elm)
+        return data
+
+    def populate_shows(self):
+        data = self.pull_data(0)
+        for document in data:
+            if self.db.shows.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+                dba.create_show(document['long_name'], document['name'])
+                print("{} will be inserted".format(document['name']))
+
+    def populate_seqs(self):
+        data = self.pull_data(1)
+        for document in data:
+            if self.db.seqs.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+                dba.create_seq(document['show'], document['name'])
+                print("{} is inserted".format(document['name']))
+
+    def populate_shots(self):
+        data = self.pull_data(2)
+        for document in data:
+            if self.db.shots.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+                dba.create_shot(document['show'], document['seq'], document['name'], frame_in=int(document.get('frame_in', 1001)), frame_out=int(document.get('frame_out', 1001)), status=document['status'], target_date=self.convert_xlsDate_to_datetime(document['target_date']))
+                print("{} is inserted".format(document['name']))
+
+    def populate_assets(self):
+        data = self.pull_data(3)
+        for document in data:
+            if self.db.assets.find({"name": document['name']}).count() > 0:
+                print("{} already exists!".format(document['name']))
+            else:
+                if document['hero'] == "Hero":
+                    hero_type = True
+                else:
+                    hero_type = False
+                dba.create_asset(document['show'], document['name'], document['type'], hero_type, self.convert_xlsDate_to_datetime(document['target_date']))
+
+    def populate_all(self):
+        self.populate_shows()
+        self.populate_seqs()
+        self.populate_shots()
+        self.populate_assets()
